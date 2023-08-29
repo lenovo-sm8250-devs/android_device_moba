d8187697e2eedc30209c2fe58667cf6d05b8c48a
Dread <dreadcreator5@gmail.com>
2023-08-29 03:50:54 +0000

    rootdir: add init.qdmastats.rc from stock
    Signed-off-by: Dread <dreadcreator5@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -102,3 +102,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qdmastats.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qdmastats.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+include $(BUILD_PREBUILT)
